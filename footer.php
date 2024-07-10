785376d956ea1d826b66843425f0e05976d5793a
Yarragudi Bhuvanesh Reddy <160226817+bhuvaneshredd@users.noreply.github.com>
2024-07-10 11:12:10 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,97 @@
+<!-- Footer ============================================= -->
+<footer id="footer" class="dark">
+
+    <div class="container">
+
+        <!-- Footer Widgets ============================================= -->
+        <div class="footer-widgets-wrap clearfix">
+
+            <div class="col_two_third">
+
+                <div class="col_one_third">
+
+                    <div class="widget clearfix">
+
+                        <img src="images/logo-footer2.png" alt="Footer Logo" class="footer-logo" style="height: 150px; width: 500px; float:right">
+
+                        <p>We believe in <strong>better Services</strong> &amp; <strong>Education</strong> is free and compulsory for all.</p>
+                        <div>
+                            <abbr title="Phone Number"><strong>Phone:</strong></abbr> 1234-123456<br>
+                            <abbr title="Email Address"><strong>Email:</strong></abbr> learncraft@gmail.com
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            
+
+        </div><!-- .footer-widgets-wrap end -->
+
+    </div>
+
+    <!-- Copyrights ============================================= -->
+    <div id="copyrights">
+
+        <div class="container clearfix">
+
+            
+
+            <div class="col_half col_last tright">
+                <div class="fright clearfix">
+
+                    <a target="_blank"  class="social-icon si-small si-borderless si-facebook">
+                        <i class="icon-facebook"></i>
+                        <i class="icon-facebook"></i>
+                    </a>
+
+                    <a target="_blank"  class="social-icon si-small si-borderless si-twitter">
+                        <i class="icon-twitter"></i>
+                        <i class="icon-twitter"></i>
+                    </a>
+
+                    <a target="_blank" class="social-icon si-small si-borderless si-youtube">
+                        <i class="icon-youtube-play"></i>
+                        <i class="icon-youtube-play"></i>
+                    </a>
+
+                    <a target="_blank" class="social-icon si-small si-borderless si-gplus">
+                        <i class="icon-gplus"></i>
+                        <i class="icon-gplus"></i>
+                    </a>
+
+                    <a target="_blank" class="social-icon si-small si-borderless si-dropbox">
+                        <i class="icon-dropbox2"></i>
+                        <i class="icon-dropbox2"></i>
+                    </a>
+
+                    <a target="_blank" class="social-icon si-small si-borderless si-github">
+                        <i class="icon-github"></i>
+                        <i class="icon-github"></i>
+                    </a>
+
+                </div>
+
+                <div class="clear"></div>
+
+                <i class="icon-envelope2"></i> learncraft@gmail.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> 123456789<span class="middot"></span>
+
+            </div>
+
+        </div>
+
+    </div><!-- #copyrights end -->
+
+</footer><!-- #footer end -->
+
+<!-- Go To Top ============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+
+<!-- External JavaScripts ============================================= -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+
+<!-- Footer Scripts ============================================= -->
+<script type="text/javascript" src="js/functions.js"></script>
---
+++ home.php
@@ -0,0 +1,258 @@
+<?php include ("header.php"); ?>
+
+<div class="container clearfix"></div>
+<!-- Page Sub Menu
+	============================================= -->
+<div id="page-menu">
+	<div id="page-menu-wrap">
+	</div>
+</div><!-- #page-menu end -->
+
+<section id="slider" class="slider-parallax swiper_wrapper full-screen clearfix">
+	<div class="slider-parallax-inner">
+		<div class="swiper-container swiper-parent">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide dark">
+					<div class="container clearfix">
+						<div class="slider-caption slider-caption-center">
+							<p data-caption-animate="fadeInUp" data-caption-delay="200"></p>
+						</div>
+					</div>
+					<div class="video-wrap">
+						<img src="images/slider/slide1.jpg">
+						<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+					</div>
+				</div>
+				<div class="swiper-slide dark"
+					style="background-image: url('images/slider/slide2.jpg'); background-position: center top;">
+					<div class="container clearfix">
+						<div class="slider-caption">
+							<p data-caption-animate="fadeInUp" data-caption-delay="200"></p>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide dark"
+					style="background-image: url('images/slider/slide3.jpg'); background-position: center top;">
+					<div class="container clearfix">
+						<div class="slider-caption">
+							<p data-caption-animate="fadeInUp" data-caption-delay="200"></p>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide dark"
+					style="background-image: url('images/slider/slide4.jpg'); background-position: center top;">
+					<div class="container clearfix">
+						<div class="slider-caption">
+							<p data-caption-animate="fadeInUp" data-caption-delay="200"></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
+			<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
+		</div>
+
+		<a href="#" data-scrollto="#content" data-offset="100" class="dark one-page-arrow"><i
+				class="icon-angle-down infinite animated fadeInDown"></i></a>
+	</div>
+</section>
+
+<!-- Content
+============================================= -->
+<section id="content">
+	<div class="content-wrap">
+		<div class="container clearfix">
+			<div class="row clearfix">
+				<div class="col-lg-5">
+					<div class="heading-block topmargin">
+						<h1>Welcome to LearnCraft Programmers!</h1>
+					</div>
+					<p class="lead">Are you a student looking to secure your future in the tech industry? Look no
+						further!
+
+						At LearnCraft Programmers, we are dedicated to promoting education and empowering learners
+						worldwide. We offer:
+
+						Free International Standard Video Lectures and Content: Access top-quality educational resources
+						at no cost.
+						Web & Software Development Services: Get professional support and development services for your
+						projects.
+						Join us today and take the first step towards a successful career in programming. Contact us now
+						to start your journey with LearnCraft Programmers!</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="section nobottommargin">
+			<div class="container clear-bottommargin clearfix">
+				<div class="row topmargin-sm clearfix">
+					<div class="col-md-4 bottommargin">
+						<i class="i-plain color i-large icon-smile inline-block" style="margin-bottom: 15px;"></i>
+						<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+							<h4>Positive Attitude</h4>
+						</div>
+						<p>We are here for achieve your expectations. Our professional programmers have care about your
+							product. They are positive, willing to go the distance to get the job done and bring their
+							best every day.</p>
+					</div>
+
+					<div class="col-md-4 bottommargin">
+						<i class="i-plain color i-large icon-users inline-block" style="margin-bottom: 15px;"></i>
+						<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+							<h4>Customer Communication Skills</h4>
+						</div>
+						<p>We have a team for communication with users to inform the status, requirements and guidelines
+							about the project in present also in future.</p>
+					</div>
+
+					<div class="col-md-4 bottommargin">
+						<i class="i-plain color i-large icon-time inline-block" style="margin-bottom: 15px;"></i>
+						<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+							<h4>Great at Time and Task Management</h4>
+						</div>
+						<p>We are highly reliable. Our team have a strong work ethic. An important skill is the ability
+							to estimate the amount of time needed to complete a task.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container clearfix" id="features">
+			<div class="row topmargin-lg bottommargin-sm">
+				<div class="heading-block center">
+					<h2>We are learncraft programmers</h2>
+					<span class="divcenter">Our team and instructors have many unique qualities &amp; skills. Dear
+						students, We are providing free online learning platform. Where your skills are polished.</span>
+				</div>
+
+				<div class="clear"></div>
+
+				<div class="section parallax dark nobottommargin" data-stellar-background-ratio="0.4">
+					<div class="heading-block center">
+						<h3>Our Heroes</h3>
+					</div>
+
+					<div class="fslider testimonial testimonial-full" data-animation="fade" data-arrows="false">
+						<div class="flexslider">
+							<div class="slider-wrap">
+								<div class="slide">
+									<div class="testi-image">
+										<a href="#"><img src="images/testimonials/1.jpg"
+												alt="Customer Testimonails"></a>
+									</div>
+									<div class="testi-content">
+										<p>We don't build services to make money; we make money to build better services
+										</p>
+										<div class="testi-meta">
+											Mark Zuckerberg
+											<span>Facebook</span>
+										</div>
+									</div>
+								</div>
+								<div class="slide">
+									<div class="testi-image">
+										<a href="#"><img src="images/testimonials/2.jpg"
+												alt="Customer Testimonails"></a>
+									</div>
+									<div class="testi-content">
+										<p>Innovation distinguishes between a leader and a follower.</p>
+										<div class="testi-meta">
+											Steve Jobs
+											<span>Apple Inc.</span>
+										</div>
+									</div>
+								</div>
+								<div class="slide">
+									<div class="testi-image">
+										<a href="#"><img src="images/testimonials/3.jpg"
+												alt="Customer Testimonails"></a>
+									</div>
+									<div class="testi-content">
+										<p>Your most unhappy customers are your greatest source of learning.</p>
+										<div class="testi-meta">
+											Bill Gates
+											<span>Microsoft</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="section notopmargin notopborder">
+					<div class="container clearfix">
+						<div class="heading-block center nomargin">
+							<h3>Latest from the Blog</h3>
+						</div>
+					</div>
+				</div>
+
+				<div class="container clear-bottommargin clearfix">
+					<div class="row">
+						<?php
+						$query = "SELECT * FROM `blog` ORDER BY id DESC limit 4";
+						$result = mysqli_query($connection, $query);
+						if (mysqli_num_rows($result) > 0) {
+							while ($row = mysqli_fetch_assoc($result)) {
+								$postId = $row['id'];
+								$status = $row["status"];
+								$post = $row["post"];
+								$title = $row["title"];
+								$content = $row["postContent"];
+								$adminPost = $row["admin"];
+								$postDate = $row["postDate"];
+
+								$query2 = "SELECT * FROM `admin` WHERE id='$adminPost'";
+								$result2 = mysqli_query($connection, $query2);
+								if (mysqli_num_rows($result2) > 0) {
+									while ($row2 = mysqli_fetch_assoc($result2)) {
+										$adminName = $row2['name'];
+									}
+								} else {
+									$adminName = 'Exceptional Programmers';
+								}
+
+								echo '<div class="col-md-3 col-sm-6 bottommargin">
+							<div class="ipost clearfix">';
+
+								if ($status == 'image') {
+									echo '<div heigth="150px" width="150px">
+									<a href="blog.php?id=' . $postId . '"><img class="image_fade" src="gotoep/images/blog/' . $post . '" alt="Image"></a>
+								</div>';
+								} else {
+									echo '<div>
+								<a href="blog.php?id=' . $postId . '"><iframe width="150px" height="150px" src="https://www.youtube.com/embed/' . $post . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+								</div>';
+								}
+								echo '<div class="entry-title">
+									<h3><a href="blog.php?id=' . $postId . '">' . $title . ' </a></h3>
+								</div>
+								
+								<ul class="entry-meta clearfix">
+									<li><i class="icon-calendar3"></i> ' . $postDate . '</li>
+									<li><i class="icon-user"></i>' . $adminName . '</li>
+								</ul>
+							
+								<div class="entry-content">
+									<p>' . $content . '</p>
+								</div>
+								</div>
+								</div>';
+							}
+						} else {
+							echo '<div class="section notopmargin notopborder">
+					<div class="container clearfix">
+						<div class="heading-block center nomargin">
+							<h3>Posts are not available Yet</h3>
+							</div>
+						</div>
+					</div>';
+						}
+						?>
+					</div>
+				</div>
+			</div>
+</section><!-- #content end -->
+
+<?php include ("footer.php"); ?>
